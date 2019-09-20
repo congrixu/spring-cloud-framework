4d1c6bcdf3a8ec0847e86bee9ddd93005d80feed
congrixu <congrixu@nalipei.com>
2019-09-20 11:37:59 +0800

    修改 mysql-connector-java版本，解决有的数据库不能自动创建ACTIVITI表的问题

--- activiti-workflow/src/main/java/com/rxv5/WorkFlowApplication.java
+++ activiti-workflow/src/main/java/com/rxv5/WorkFlowApplication.java
@@ -3,7 +3,6 @@ package com.rxv5;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 工作流单体工程
@@ -15,7 +14,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
 		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class })
-@EnableEurekaClient
 public class WorkFlowApplication {
 
 	public static void main(String[] args) {
