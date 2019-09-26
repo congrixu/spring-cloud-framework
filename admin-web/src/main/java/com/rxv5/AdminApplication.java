beb44f5d51460da3ed9e23d86be10cbd95046b8b
congrixu <congrixu@nalipei.com>
2019-09-26 12:27:32 +0800

    nothing

--- admin-web/src/main/java/com/rxv5/AdminApplication.java
+++ admin-web/src/main/java/com/rxv5/AdminApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 注意当前类所在的位置。要与调用feign同包或者父包下。否则@EnableFeignClients添加basePackages
+ * @EnableFeignClients(basePackages="")
+ * @author congrixu
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
